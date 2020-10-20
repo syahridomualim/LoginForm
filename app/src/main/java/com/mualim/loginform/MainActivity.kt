8f78008c20771bb817019aa0f5f78c7dac6f425c
Mu'alim Syahrido <syahridomualim@gmail.com>
2020-10-20 07:48:08 +0700

    LoginForm

---
+++ app/src/main/java/com/mualim/loginform/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.mualim.loginform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var edtUsername: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var tvRegister: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        edtUsername = findViewById(R.id.edt_username)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btn_login)
+        tvRegister = findViewById(R.id.tv_register)
+
+        btnLogin.setOnClickListener {
+
+        }
+
+    }
+}
